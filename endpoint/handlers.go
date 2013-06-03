097632d4868647926a59e27a876487a4d474e2ba
Song Gao <song@gao.io>
2013-06-03 13:23:37 -0700

    added more debuging output

--- endpoint/handlers.go
+++ endpoint/handlers.go
@@ -11,6 +11,7 @@ func (s *Server) handleHeartbeat(req *request, encoder *json.Encoder, decoder *j
 	err := json.Unmarshal(req.Params, &hb)
 	if err != nil {
 		rsp.Err = getErr(err)
+		logger.Printf("Unmarshaling heartbeat error: %v\n", err)
 	} else {
 		logger.Printf("Got a heartbeat: %v\n", hb.Timestamp)
 		rsp.Result, _ = Heartbeat{Timestamp: time.Now()}.MarshalJSON()
--- testing/vm/O_O/integration/test.sh
+++ testing/vm/O_O/integration/test.sh
@@ -11,7 +11,7 @@ monitoring_upgrade true
 
 cmd1="/usr/bin/stud -q $(dirname $0)/test.pem -b 127.0.0.1,50050 -f *,50051 --ssl --write-proxy"
 cmd2="$endpoint_bin ':50050'"
-cmd3="$agent_out/rackspace-monitoring-agent -i --zip $agent_out/virgo-bundle.zip --config /tmp/cfg"
+cmd3="$agent_out/rackspace-monitoring-agent -i --debug --zip $agent_out/virgo-bundle.zip --config /tmp/cfg"
 
 pingpong -log "/data/O_O/logs/$(date)" -- "$cmd1" "$cmd2" "$cmd3"   &
 pid=$?
