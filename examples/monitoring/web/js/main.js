196e0bfb50429582535f9bb7342a2c683dc48fe6
Song Gao <song@gao.io>
2013-06-18 17:52:32 -0700

    remove extra console.log

--- examples/monitoring/web/js/main.js
+++ examples/monitoring/web/js/main.js
@@ -1,8 +1,5 @@
 require(["d3.v3.min"], function() {
   require(["cubism.v1.min", "ctl"], function() {
-
-    console.log(d3);
-    console.log(cubism);
     var ctl = new Ctl();
     d3.json("/config", function(config) {
       ctl.config_cubism(config);
