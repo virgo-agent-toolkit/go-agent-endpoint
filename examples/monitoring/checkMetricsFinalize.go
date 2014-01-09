2ab3cff1462ea921fbbedea116e05a903027151a
Song Gao <song@gao.io>
2014-01-08 18:02:18 -0600

    changed names and import paths for moving the repo; removed git submodule init in puppet due to a bug in git 1.7.9 that uses abs path in config of submodules. removing submodule init from puppet transfers the responsibility of init submodules from vm to host, i.e. user would need to clone recursively or init submodules manually.

--- examples/monitoring/checkMetricsFinalize.go
+++ examples/monitoring/checkMetricsFinalize.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/racker/go-agent-endpoint/endpoint"
+    "github.com/virgo-agent-toolkit/go-agent-endpoint/endpoint"
 )
 
 type checkMetricsFinalizeHandler byte
 
 // Handle finalizes check_metrics.post request, writing a non-error response
 func (c checkMetricsFinalizeHandler) Handle(req *endpoint.Request, responder *endpoint.Responder, connCtx endpoint.ConnContext) endpoint.HandleCode {
-	responder.Respond(0, nil)
-	return endpoint.OK
+    responder.Respond(0, nil)
+    return endpoint.OK
 }
--- examples/monitoring/main.go
+++ examples/monitoring/main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"github.com/racker/go-agent-endpoint/endpoint"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
+
+    "github.com/virgo-agent-toolkit/go-agent-endpoint/endpoint"
 )
 
 func printUsage() {
-	fmt.Printf("Usage: %s laddr upgradingServerAddr\n", os.Args[0])
+    fmt.Printf("Usage: %s laddr upgradingServerAddr\n", os.Args[0])
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		printUsage()
-		os.Exit(1)
-	}
+    if len(os.Args) < 2 {
+        printUsage()
+        os.Exit(1)
+    }
 
-	hub, requesters := endpoint.NewHub()
-	hub.Authenticator(authenticator(0), 0)
-	hub.Hook("check_schedule.get", checkScheduleHandler(0), 0)
-	hub.Hook("check_metrics.post", checkMetricsPrintHandler(0), 0)
-	hub.Hook("check_metrics.post", newCheckMetricsWebUIHandler(":8088"), 1)
-	hub.Hook("check_metrics.post", checkMetricsFinalizeHandler(0), 8)
+    hub, requesters := endpoint.NewHub()
+    hub.Authenticator(authenticator(0), 0)
+    hub.Hook("check_schedule.get", checkScheduleHandler(0), 0)
+    hub.Hook("check_metrics.post", checkMetricsPrintHandler(0), 0)
+    hub.Hook("check_metrics.post", newCheckMetricsWebUIHandler(":8088"), 1)
+    hub.Hook("check_metrics.post", checkMetricsFinalizeHandler(0), 8)
 
-	go proactive(requesters)
+    go proactive(requesters)
 
-	config := endpoint.EndpointConfig{}
-	config.ListenAddr = os.Args[1]
-	config.UpgradingFileServerAddr = os.Args[2]
-	config.Hub = hub
+    config := endpoint.EndpointConfig{}
+    config.ListenAddr = os.Args[1]
+    config.UpgradingFileServerAddr = os.Args[2]
+    config.Hub = hub
 
-	server, err := endpoint.NewEndpoint(config)
-	if err != nil {
-		printUsage()
-		log.Fatalln(err)
-	}
-	server.Start()
-	select {}
+    server, err := endpoint.NewEndpoint(config)
+    if err != nil {
+        printUsage()
+        log.Fatalln(err)
+    }
+    server.Start()
+    select {}
 }
